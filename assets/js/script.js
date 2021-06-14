5631c51f3b30b6ada81ab090dfad8413d339caf7
Kamil Safin <kml93@msn.com>
2021-06-14 15:01:50 +0200

    Init-project with randomly content index.html style.scss script.js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.write('<h2>Hello World!</h2>');
+document.write('<p>This is my first JS script.</p>');
+document.write('<p>It is <strong>amazing!!!</strong></p>');
+document.write('<iframe src="https://www.youtube-nocookie.com/embed/IoULsBN9xH4?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
