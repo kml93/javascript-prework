96b1cd0b3b0f04a298520de9a54ffc938a857edc
Kamil Safin <kml93@msn.com>
2021-06-14 20:54:14 +0200

    Add random number script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomFraction = Math.random();
+printMessage('Wylosowany ulamek to: ' + randomFraction);
 
+let calculation = randomFraction * 3 + 1;
+printMessage('Ułamek pomnożony przez 3 i powiekszony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+printMessage('Liczba po zaokragleniu d dol to: ' + roundNumber);
+
+
+let computerMove = 'kamień';
+let playerMove = 'papier';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
 // document.write('<h2>Hello World!</h2>');
