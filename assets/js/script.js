5096725a83637d9ac8679d7830f3a6ec9ae63c1e
Kamil Safin <kml93@msn.com>
2021-06-16 22:37:59 +0200

    Add prompt & computerMove script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,48 @@ printMessage('Liczba wygenerowana to :' + randomNumber); // liczby od 11 do 19
 
 printMessage('Wylosowany ulamek to: ' + randomFraction);
 
-let calculation = randomFraction * 3 + 1;
+let calculation = (randomFraction * 3 + 1);
 printMessage('Ułamek pomnożony przez 3 i powiekszony o 1: ' + calculation);
 
 let roundNumber = Math.floor(calculation);
 printMessage('Liczba po zaokragleniu w dol to: ' + roundNumber);
 
-let computerMove = 'kamień';
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if (roundNumber == 1) {
+    computerMove = 'kamień';
+} else if (roundNumber == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch'
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else {
+    playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
+
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + ', to wygrywasz!');
+
+// if (1 > 2) {
+//     printMessage('Niesamote Jeden jest wiekszy od dwa!!');
+// } else if (1 == 2) {
+//     printMessage('Dziwne - jeden jest rowne dwa?!');
+// } else {
+//     printMessage('Wszystko po staremu jeden jest mniejszy od dwa.');
+// }
 
 // document.write('<h2>Hello World!</h2>');
 // document.write('<p>This is my first JS script.</p>');
